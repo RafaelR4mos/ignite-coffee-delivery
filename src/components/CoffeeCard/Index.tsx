9c6e5025b8a230a4c1de1dcb336b315e158eff46
Rafael <rafaelramos0401@example.com>
2023-07-31 21:51:47 -0300

    wip in coffee card

--- src/components/CoffeeCard/Index.tsx
+++ src/components/CoffeeCard/Index.tsx
@@ -1,14 +1,22 @@
 import { ICoffeeData } from "../../utils/commonInterfaces";
+import { CoffeeCardContainer, CoffeeName, CoffeeTag } from "./styles";
 
 interface CoffeCardProps {
-  coffeeData?: ICoffeeData;
+  coffeeData: ICoffeeData;
 }
 
 export function CoffeeCard({ coffeeData }: CoffeCardProps) {
   console.log(coffeeData);
   return (
-    <div>
-      <h1>Card</h1>
-    </div>
+    <CoffeeCardContainer>
+      <img src={coffeeData.coffeeImg} alt={coffeeData.name} />
+      <div>
+        {coffeeData.tags.map((tag) => (
+          <CoffeeTag>{tag}</CoffeeTag>
+        ))}
+      </div>
+      <CoffeeName>{coffeeData.name}</CoffeeName>
+      <p>{coffeeData.description}</p>
+    </CoffeeCardContainer>
   );
 }
---
+++ src/components/CoffeeCard/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const CoffeeCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  border-radius: 6px 36px 6px 36px;
+  background-color: ${(props) => props.theme["base-card"]};
+  padding: 0 1.25rem 1.25rem;
+
+  & img {
+    margin-top: -32px;
+  }
+`;
+
+export const CoffeeName = styled.strong`
+  margin-top: calc(20px - 8px);
+  font-size: 20px;
+  font-weight: 700;
+  font-family: "Baloo 2", sans-serif;
+  line-height: 130%;
+`;
+
+export const CoffeeTag = styled.span`
+  border-radius: 100px;
+  padding: 4px 8px;
+  background-color: ${(props) => props.theme["yellow-light"]};
+
+  font-size: 12px;
+  font-weight: 700;
+  color: ${(props) => props.theme["yellow-dark"]};
+`;
