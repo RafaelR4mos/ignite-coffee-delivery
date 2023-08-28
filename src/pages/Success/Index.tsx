b98dae5bac01f2fe8042dd3bcfd1e195877771e3
Rafael <rafaelramos0401@example.com>
2023-08-28 12:13:27 -0300

    add: success page now dinamic

--- src/pages/Success/Index.tsx
+++ src/pages/Success/Index.tsx
@@ -1,11 +1,16 @@
 import { CartContext } from "../../context/Cart";
 import { useContext } from "react";
 import {
+  DeliveryInfoRow,
+  ImgContainer,
   OrderInfo,
   OrderInfoAndImageContainer,
   SuccessPageContainer,
   TitleContainer,
 } from "./styles";
+
+import motoboyImg from "../../assets/motoboy.svg";
+import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
 
 export function Success() {
   const { coffeeOrder } = useContext(CartContext);
@@ -21,18 +26,51 @@ export function Success() {
 
       <OrderInfoAndImageContainer>
         <OrderInfo>
-          <div>
-            <span>a</span>
+          <DeliveryInfoRow $iconBgColor="purple">
+            <span>
+              <MapPin size={16} weight="fill" />
+            </span>
             <div>
-              <p>Entrega em .... ,</p>
-              <p>Cidade...</p>
+              <p>
+                Entrega em
+                <strong>{` ${coffeeOrder?.street}, ${coffeeOrder?.number}`}</strong>
+              </p>
+              <p>{`${coffeeOrder?.neighborhood} - ${coffeeOrder?.city}, ${coffeeOrder?.uf}`}</p>
             </div>
-          </div>
+          </DeliveryInfoRow>
+
+          <DeliveryInfoRow $iconBgColor="yellow">
+            <span>
+              <Timer size={16} weight="fill" />
+            </span>
+            <div>
+              <p>Previsão de entrega</p>
+              <p>
+                <strong>20 min - 30 min</strong>
+              </p>
+            </div>
+          </DeliveryInfoRow>
+
+          <DeliveryInfoRow $iconBgColor="yellow-dark">
+            <span>
+              <CurrencyDollar size={16} weight="fill" />
+            </span>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p>
+                {coffeeOrder?.payment === "dinheiro"
+                  ? `${coffeeOrder.payment}`
+                  : `Cartão de ${coffeeOrder?.payment}`}
+              </p>
+            </div>
+          </DeliveryInfoRow>
         </OrderInfo>
-        <div>
-          <img src="" alt="" />
-          <h2>a</h2>
-        </div>
+        <ImgContainer>
+          <img
+            src={motoboyImg}
+            alt="imagem de um entregador em uma moto, saindo com uma encomenda."
+          />
+        </ImgContainer>
       </OrderInfoAndImageContainer>
     </SuccessPageContainer>
   );
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -23,19 +23,87 @@ export const TitleContainer = styled.div`
 
 export const OrderInfoAndImageContainer = styled.div`
   display: grid;
+  align-items: stretch;
+  justify-content: space-between;
   grid-template-columns: 1fr 1fr;
   gap: 7rem;
+  max-height: 300px;
 `;
 
 export const OrderInfo = styled.div`
+  //border styles
+  --linear-gradient: linear-gradient(
+    to left,
+    rgba(128, 71, 248, 1),
+    rgba(219, 172, 44, 1)
+  );
+  --border-width: 1px;
+  --border-radius: 6px 36px;
+
+  background: var(--linear-gradient);
+  border: 0;
+  border-radius: var(--border-radius);
+
   padding: 2.5rem;
+  position: relative;
+  z-index: 1;
+  align-self: stretch;
+
   display: flex;
   flex-direction: column;
   gap: 2rem;
-  border-radius: 6px 36px;
-  background: linear-gradient(
-    90deg,
-    rgba(219, 172, 44, 1) 0%,
-    rgba(128, 71, 248, 1) 100%
-  ); /* Apply the gradient border */
+
+  &::before {
+    content: "";
+    display: block;
+
+    height: calc(100% - calc(var(--border-width) * 2));
+    width: calc(100% - calc(var(--border-width) * 2));
+
+    background-color: ${(props) => props.theme.background};
+    position: absolute;
+
+    top: var(--border-width);
+    left: var(--border-width);
+
+    border-radius: calc(6px - var(--border-width))
+      calc(36px - var(--border-width));
+    z-index: -1;
+  }
+`;
+
+export const ImgContainer = styled.div`
+  & > img {
+    max-height: 18.75rem;
+    max-width: 100%;
+  }
+`;
+
+type iconBgColorProps = "purple" | "yellow" | "yellow-dark";
+
+export const DeliveryInfoRow = styled.div<{ $iconBgColor: iconBgColorProps }>`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
+
+  & > span {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    color: ${(props) => props.theme.white};
+    background-color: ${(props) => props.theme[props.$iconBgColor]};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    p {
+      color: ${(props) => props.theme["base-text"]};
+    }
+  }
 `;
